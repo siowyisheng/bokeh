8c4e6070555dfed8ce45107024a1556935365fd1
Philipp Rudiger <prudiger@anaconda.com>
2019-12-28 01:47:43 +0100

    Added new options

--- bokeh/models/widgets/inputs.py
+++ bokeh/models/widgets/inputs.py
@@ -21,6 +21,7 @@ log = logging.getLogger(__name__)
 # Bokeh imports
 from ...core.has_props import abstract
 from ...core.properties import (
+    Bool,
     ColorHex,
     Date,
     Dict,
@@ -46,9 +47,9 @@ __all__ = (
     'DatePicker',
     'FileInput',
     'InputWidget',
+    'MultiChoice',
     'MultiSelect',
     'PasswordInput',
-    'Pillbox',
     'Select',
     'Spinner',
     'TextInput',
@@ -250,8 +251,8 @@ class MultiSelect(InputWidget):
     """)
 
 
-class Pillbox(InputWidget):
-    ''' Pillbox widget.
+class MultiChoice(InputWidget):
+    ''' MultiChoice widget.
 
     '''
 
@@ -265,6 +266,21 @@ class Pillbox(InputWidget):
     value = List(String, help="""
     Initial or selected values.
     """)
+
+    delete_button = Bool(default=True, help="""
+    Whether to add a button to remove a selected option.""")
+
+    max_items = Int(default=None, help="""
+    The maximum number of items that can be selected.
+    """)
+
+    option_limit = Int(default=None, help="""
+    The number of choices that will be rendered in the dropdown.
+    """)
+
+    placeholder = String(default=None, help="""
+    A string that is displayed if not item is added.""")
+
 
 
 class DatePicker(InputWidget):
